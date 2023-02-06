edcd8e44c5a73fda3c8360d4771d724629f43fa0
kev2010 <cloudwiz24@gmail.com>
2023-02-06 15:24:29 -0500

    im dumb - changing aspect ratio

--- components/UploadImage.js
+++ components/UploadImage.js
@@ -9,7 +9,7 @@ const UploadImage = ({ image, updateImage }) => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
-      aspect: [4, 3],
+      aspect: [4, 4],
       quality: 0.2,
     });
 
