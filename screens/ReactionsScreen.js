ae670d63dc7ade6e3132e06afab66ef647929a82
kev2010 <cloudwiz24@gmail.com>
2023-02-18 00:55:31 -0500

    fix comments loading UI bug

--- screens/ReactionsScreen.js
+++ screens/ReactionsScreen.js
@@ -27,6 +27,7 @@ const ReactionsScreen = ({ navigation, route }) => {
   const [data, setData] = useState([]);
   const [initialLoading, setInitialLoading] = useState(true);
   const [emojiCount, setEmojiCount] = useState(route.params.emojis);
+  const [loading, setLoading] = useState(false);
   // TODO: Unclutter like in HomeScreen by moving bottom sheet to a separate component
   const goBack = () => {
     navigation.goBack();
@@ -35,6 +36,7 @@ const ReactionsScreen = ({ navigation, route }) => {
   // TODO: This is an extremely hacky solution. For some reason, the Bottom Sheet component I used online (screw outside components holy crap) calls onChange on render, which is why we have to pass in !swiped instead of swiped. E.g. if we pass "swiped" (initialized to false) to the components, then, ON RENDER, onChange is fired, which calls "handleBottomSheetSwipe" immediately, which then changed swiped to true. This means that the page effectively loads with swiped=true so the keyboard pops up.
   // But the problem with just passing !swiped is that the keyboard is initially set to true (because !swiped = true is being passed in to GiveComment), so the keyboard initially pops up before the render onChange is fired and !swiped becomes false, so the keyboard goes back down. My temporary hack to fix this keyboard popping up in the beginning is to just add a "loading" thing for 450ms that hides everything (giving swiped a chance to change around)before showing everything. Should definitely fix in the future!
   useEffect(() => {
+    setLoading(true);
     refreshReactions();
     setTimeout(() => {
       setInitialLoading(false);
@@ -46,33 +48,47 @@ const ReactionsScreen = ({ navigation, route }) => {
   }, []);
 
   const refreshReactions = () => {
+    let itemsProcessed = 0;
     getReactions(route.params.id).then((reactions) => {
-      reactions.forEach((reactionDoc) => {
-        getUser(reactionDoc.data().name.id).then((user) => {
-          getProfilePicture(reactionDoc.data().name.id).then((imageURL) => {
-            const found = data.some(
-              (reaction) => reaction.id === reactionDoc.id
-            );
-            if (!found) {
-              // IMPORTANT: Need to use a function to create a new array since state updates are asynchronous or sometimes batched.
-              setData((data) => [
-                {
-                  id: reactionDoc.id,
-                  creatorID: user.id,
-                  imageURL: imageURL,
-                  name: user.data().username,
-                  text: reactionDoc.data().text,
-                  time: calculateTimeDiffFromNow(
-                    reactionDoc.data().time.toDate()
-                  ),
-                  rawTime: reactionDoc.data().time,
-                },
-                ...data,
-              ]);
-            }
+      if (reactions.size > 0) {
+        reactions.forEach((reactionDoc) => {
+          getUser(reactionDoc.data().name.id).then((user) => {
+            getProfilePicture(reactionDoc.data().name.id).then((imageURL) => {
+              const found = data.some(
+                (reaction) => reaction.id === reactionDoc.id
+              );
+              if (!found) {
+                // IMPORTANT: Need to use a function to create a new array since state updates are asynchronous or sometimes batched.
+                setData((data) => [
+                  {
+                    id: reactionDoc.id,
+                    creatorID: user.id,
+                    imageURL: imageURL,
+                    name: user.data().username,
+                    text: reactionDoc.data().text,
+                    time: calculateTimeDiffFromNow(
+                      reactionDoc.data().time.toDate()
+                    ),
+                    rawTime: reactionDoc.data().time,
+                  },
+                  ...data,
+                ]);
+                itemsProcessed++;
+                if (itemsProcessed === reactions.size) {
+                  setLoading(false);
+                }
+              } else {
+                itemsProcessed++;
+                if (itemsProcessed === reactions.size) {
+                  setLoading(false);
+                }
+              }
+            });
           });
         });
-      });
+      } else {
+        setLoading(false);
+      }
     });
   };
 
@@ -160,13 +176,21 @@ const ReactionsScreen = ({ navigation, route }) => {
             locked={false}
           />
           <View style={styles.reactions}>
-            <ReactionSection
-              navigation={navigation}
-              data={data.sort(function (x, y) {
-                return x.rawTime - y.rawTime;
-              })}
-              uid={route.params.id}
-            />
+            {loading ? (
+              <ActivityIndicator
+                style={{ marginTop: 24 }}
+                size="large"
+                color={colors.primary_5}
+              />
+            ) : (
+              <ReactionSection
+                navigation={navigation}
+                data={data.sort(function (x, y) {
+                  return x.rawTime - y.rawTime;
+                })}
+                uid={route.params.id}
+              />
+            )}
           </View>
         </ScrollView>
         <BottomSheet
